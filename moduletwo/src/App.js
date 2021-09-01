c9702ac6237e7338b4ea761ded72ba07d01b95f7
vaneo0814 <vorellana814@gmail.com>
2021-09-01 14:08:45 -0400

    created components successfully

--- moduletwo/src/App.js
+++ moduletwo/src/App.js
@@ -1,15 +1,45 @@
 import { GlobalStyle } from './utils';
 import { Button } from './components/Button';
+import { Logo } from './components/Logo';
+import { Header, Nav, NavItem } from './components/Nav';
+
 
 
 const App = () => {
   return (
     <main>
       <GlobalStyle />
-      <Button onClick={() => alert('Triggered by button click!')}>This is a default button</Button>
+      <Header>
+        <Logo />
+        <Nav>
+          <NavItem href='#'>Shop</NavItem>
+          <NavItem href='#'>Blog</NavItem>
+          <NavItem href='#'>Location</NavItem>
+          <NavItem href='#'>Contact</NavItem>
+        </Nav>
+      </Header>
+
+      <Header variant='right'>
+        <Logo variant='secondary' href='#' />
+        <Nav>
+          <NavItem href='#'>Shop</NavItem>
+          <NavItem href='#'>Blog</NavItem>
+          <NavItem href='#'>Location</NavItem>
+          <NavItem href='#'>Contact</NavItem>
+        </Nav>
+      </Header>
+
+      <Button href='http://google.com'>This is a button with a link</Button>
       <br />
       <br />
-      <Button href='http://google.com'>This is a default button with a link</Button>
+      <Button variant='secondary' size='large' disabled>
+        This is a secondary disabled button
+      </Button>
+      <br />
+      <br />
+      <Button variant='tertiary' size='small'>
+        This is a tertiary button
+      </Button>
     </main>
   );
 };
